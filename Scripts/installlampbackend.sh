178b73d5b9f866dd52c4521caae15de0e78c5cd2
samuel <samuel@gmail.com>
2023-11-15 13:15:49 +0000

    "direccion ip privada backend"

--- Scripts/installlampbackend.sh
+++ Scripts/installlampbackend.sh
@@ -13,3 +13,13 @@ apt update
 
 apt install mysql-server -y
 
+#.env
+
+source .env
+
+# configuramos mysql  para que acepete conxiones desde la ip privada 
+
+sed -i "s/127.0.0.1/$MYSQL_PRIVATE/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+
+
